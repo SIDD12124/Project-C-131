31c3f65b6ec3cfa38ec550a845e2b4a3ec81668b
SIDD12124 <72179881+SIDD12124@users.noreply.github.com>
2021-07-24 17:27:46 +0530

    Add files via upload

---
+++ aquarium.js
@@ -0,0 +1,52 @@
+img = "";
+status = "";
+object = [];
+
+function preload(){
+
+    img = loadImage("https://i.postimg.cc/3w7Y7jpy/Aquarium.jpg");
+}
+
+function at_home(){
+    window.location = "index.html";
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.position(370,180);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    } else {
+        console.log(results);
+        object = results;
+    }
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    
+    if(status != ""){
+        for (var i = 0; i<object.length; i++) {
+            
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x, object[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y - 90, object[i].width, object[i].height);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function bed_room(){
+    window.location = "bed_room.html";
+}
+
+function living_Room(){
+    window.location = "living_room.html";
+}
+
+function Indoor_swing(){
+    window.location = "birds.html";
+}
+
+function Aquarium(){
+    window.location = "aquarium.html";
+}
+
+function tv(){
+    window.location = "tv.html";
+}
